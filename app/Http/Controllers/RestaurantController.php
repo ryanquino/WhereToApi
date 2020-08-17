e7add6bb4ac794c95cece8dd5ea95adf6cedbb3a
ryanquino <ryanquino@gmail.com>
2020-08-17 21:00:34 +0800

    fixed addResto

--- app/Http/Controllers/RestaurantController.php
+++ app/Http/Controllers/RestaurantController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Restaurant;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 class RestaurantController extends Controller
 {
     /**
