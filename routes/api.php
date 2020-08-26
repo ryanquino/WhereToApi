88667a5ef2cece223b489f3d2032b77a15df7686
ryanquino <ryanquino@gmail.com>
2020-08-26 16:31:07 +0800

    edited

--- routes/api.php
+++ routes/api.php
@@ -60,6 +60,7 @@ Route::get('viewUserVerification/{id}', 'VerificationController@viewUserVerifica
 Route::get('viewRiderRemittance', 'RemitController@viewRiderRemittance');
 Route::get('viewUnremittedList', 'RemitController@viewUnremittedList');
 Route::get('viewRemittedList', 'RemitController@viewRemittedList');
+Route::get('checkRiderRemittance', 'RemitController@checkRiderRemittance');
 Route::post('getUserVerification/{id}', 'VerificationController@getUserVerification');
 Route::post('updateVerification', 'VerificationController@updateVerification');
 Route::get('checkVerification', 'VerificationController@checkVerification');
