64d50c5547669b0ed2b70371e0c4976ebd0fdfcb
ryanquino <ryanquino@gmail.com>
2020-08-26 15:07:55 +0800

    edited

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -61,7 +61,7 @@ class UserController extends Controller
     public function login(Request $request)
     {
         $credentials = $request->json()->all();
-        // try {
+        try {
             if (! $token = JWTAuth::attempt($credentials)) {
                 return response()->json([
                     'message' => false,
@@ -83,9 +83,9 @@ class UserController extends Controller
                 }
             }
             
-        // } catch (JWTException $e) {
-        //     return response()->json(['error' => 'could_not_create_token'], 500);
-        // }
+        } catch (JWTException $e) {
+            return response()->json(['error' => 'could_not_create_token'], 500);
+        }
         return response()->json([
             'success'=> true,
             'user'=> $user,
