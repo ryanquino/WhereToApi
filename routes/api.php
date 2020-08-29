44e0f9ab7735d1dc073f14eac0c14642f304a46d
ryanquino <ryanquino@gmail.com>
2020-08-29 15:15:19 +0800

    edited

--- routes/api.php
+++ routes/api.php
@@ -72,7 +72,7 @@ Route::get('checkVerification', 'VerificationController@checkVerification');
 Route::post('unSuspendRider/{id}', 'UserController@unSuspendRider');
 Route::post('suspendRider/{id}', 'UserController@suspendRider');
 Route::post('updateMenu', 'MenuController@updateMenu');
-Route::post('updateRestaurant', 'MenuController@updateRestaurant');
+Route::post('updateRestaurant', 'RestaurantController@updateRestaurant');
 Route::post('makeMenuFeatured/{id}', 'MenuController@makeMenuFeatured');
 Route::post('makeRestaurantFeatured/{id}', 'RestaurantController@makeRestaurantFeatured');
 
