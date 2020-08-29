6f5ecb2afdcc0a17774b225ece6ceabfeb7b81b4
ryanquino <ryanquino@gmail.com>
2020-08-29 23:00:06 +0800

    edited

--- app/Http/Controllers/RestaurantController.php
+++ app/Http/Controllers/RestaurantController.php
@@ -143,17 +143,14 @@ class RestaurantController extends Controller
         //
     }
 
-    // public function getFeaturedRestaurant(){
-    //     $resto = DB::table('restaurant')
-    //         ->join('barangay', 'barangay.id', '=', 'restaurants.barangayId')
-    //         ->select('restaurants.id', 'restaurants.restaurantName', 'restaurants.address', 'barangay.barangayName', 'restaurants.contactNumber', 'restaurants.imagePath')
-    //         ->
-    //     // $featuredResto = DB::table('restaurants')
-    //     //     ->select('restaurants.id', 'restaurants.restaurantName', 'restaurants.address', 'barangay.barangayName', 'restaurants.contactNumber', 'restaurants.imagePath')
-    //     //     ->join('barangay', 'barangay.id', '=', 'restaurants.barangayId')
-    //     //     ->where('isFeatured', 1);
-    //     // return response()->json($featuredResto);
-    // }
+    public function getFeaturedRestaurant(){
+        $featuredResto = DB::table('restaurants')
+            ->select('restaurants.id', 'restaurants.restaurantName', 'restaurants.address', 'barangay.barangayName', 'restaurants.contactNumber', 'restaurants.imagePath')
+            ->join('barangay', 'barangay.id', '=', 'restaurants.barangayId')
+            ->where('isFeatured', 1)
+            ->where('isActive', 1)->get();
+        return response()->json($featuredResto);
+    }
 
     public function getRestaurantById($id){
         $resto = Restaurant::where('id', $id)->where('isActive', 1)->first();
