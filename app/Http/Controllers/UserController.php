36cb707effe91d8e74a4e1d95cf1cf1800b65da9
ryanquino <ryanquino@gmail.com>
2020-08-30 23:08:46 +0800

    edited

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -179,7 +179,7 @@ class UserController extends Controller
                 ->select(DB::raw('date(created_at)'))
                 ->latest()
                 ->first();
-return response()->json($date->created_at == date('Y-m-d'));
+return response()->json($date->created_at);
 
 
             $remitStatus = DB::select('SELECT imagePath from remittance where riderId = ? and date(created_at) = CURDATE()-1', [$id]);
