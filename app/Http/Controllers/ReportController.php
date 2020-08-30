10a722af30cc4fd7ca1aa7e8fcbab961dd9a9d96
ryanquino <ryanquino@gmail.com>
2020-08-30 23:04:57 +0800

    edited

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -8,8 +8,17 @@ class ReportController extends Controller
 {
     //
     public function getRestaurantSalesReport(Request $request){
+    	$restaurantId = $request->json()->get('restaurantId');
+    	$dateFrom = $request->json()->get('dateFrom');
+    	$dateTo = $request->json()->get('dateTo');
     	$report = DB::table('transactions')
     		->join('food_orders', 'food_orders.transactionId', '=', 'transactions.id')
-    		->select()->get();
+    		->join('menu', 'food_orders.menuId', '=', 'menu.id')
+    		->select(DB::raw('transactions.id, transactions.deliveryAddress, menu.menuName, menu.price, food_orders.quantity, (menu.price *food_orders.quantity) as total'))
+    		->where('transactions.restaurantId', $restaurantId)
+    		->whereBetween('transactions.created_at', [$dateFrom, $dateTo])
+    		->get();
+
+    	return response()->json($report);
     }
 }
