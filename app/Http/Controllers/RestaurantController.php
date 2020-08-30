57a2e7ff4277203200f5aeaf434432a9353ca351
ryanquino <ryanquino@gmail.com>
2020-08-30 08:00:03 +0800

    edited

--- app/Http/Controllers/RestaurantController.php
+++ app/Http/Controllers/RestaurantController.php
@@ -144,12 +144,10 @@ class RestaurantController extends Controller
     }
 
     public function getFeaturedRestaurant(){
-        $featuredResto = DB::table('restaurants')
-            ->select('restaurants.id', 'restaurants.restaurantName', 'restaurants.address', 'barangay.barangayName', 'restaurants.contactNumber', 'restaurants.imagePath')
-            ->join('barangay', 'barangay.id', '=', 'restaurants.barangayId')
-            ->where('isFeatured', 1)
+        $resto = Restaurant::where('isFeatured', 1)
             ->where('isActive', 1)->get();
-        return response()->json($featuredResto);
+
+        return response()->json($resto);
     }
 
     public function getRestaurantById($id){
