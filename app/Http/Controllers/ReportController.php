f976b26089c059284f44f11f2968a68e85afb24c
ryanquino <ryanquino@gmail.com>
2020-08-31 21:10:55 +0800

    edited

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -12,15 +12,15 @@ class ReportController extends Controller
     	$dateFrom = $request->json()->get('dateFrom');
     	$dateTo = $request->json()->get('dateTo');
 
-    	$report = DB::table('transactions')
-    		->join('food_orders', 'food_orders.transactionId', '=', 'transactions.id')
-    		->join('menu', 'food_orders.menuId', '=', 'menu.id')
-    		->select(DB::raw('transactions.id, transactions.deliveryAddress, menu.menuName, menu.price, food_orders.quantity, (menu.price *food_orders.quantity) as total'))
-    		->where('transactions.restaurantId', $restaurantId)
-    		->whereBetween('transactions.created_at', [$dateFrom, $dateTo])
-    		->get();
+    	// $report = DB::table('transactions')
+    	// 	->join('food_orders', 'food_orders.transactionId', '=', 'transactions.id')
+    	// 	->join('menu', 'food_orders.menuId', '=', 'menu.id')
+    	// 	->select(DB::raw('transactions.id, transactions.deliveryAddress, menu.menuName, menu.price, food_orders.quantity, (menu.price *food_orders.quantity) as total'))
+    	// 	->where('transactions.restaurantId', $restaurantId)
+    	// 	->whereBetween('transactions.created_at', [$dateFrom, $dateTo])
+    	// 	->get();
 
-    	// $report = DB::select('Select transactions.id, transactions.deliveryAddress, menu.menuName, menu.price, food_orders.quantity, (menu.price *food_orders.quantity) as total from transactions join food_orders on food_orders.transactionId = transactions.id join menu on menu.id = food_orders.menuId where transactions.restaurantId = ? and date(transactions.created_at) BETWEEN ? and ?', [$restaurantId, $dateFrom, $dateTo]);
+    	$report = DB::select('Select transactions.id, transactions.deliveryAddress, menu.menuName, menu.price, food_orders.quantity, (menu.price *food_orders.quantity) as total from transactions join food_orders on food_orders.transactionId = transactions.id join menu on menu.id = food_orders.menuId where transactions.restaurantId = ? and date(transactions.created_at) BETWEEN ? and ?', [$restaurantId, $dateFrom, $dateTo]);
 
     	return response()->json($report);
     }
