d3f6ed64a53aaa35db16b502961586a2f070a334
ryanquino <ryanquino@gmail.com>
2020-09-06 11:18:27 +0800

    edited

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
   <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <title>WhereTo Application</title>
   <meta content="" name="descriptison">
