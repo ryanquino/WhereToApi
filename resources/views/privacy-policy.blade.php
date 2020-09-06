f04535b5cfca7c8beb25a060ac46b03b9519c0a1
ryanquino <ryanquino@gmail.com>
2020-09-06 09:46:27 +0800

    edited

--- resources/views/privacy-policy.blade.php
+++ resources/views/privacy-policy.blade.php
@@ -42,13 +42,13 @@
   <header id="header" class="fixed-top">
     <div class="container d-flex align-items-center">
 
-      <h1 class="logo mr-auto"><a href="index.html">WhereTo</a></h1>
+      <h1 class="logo mr-auto"><a href="{{url('welcome')}}">WhereTo</a></h1>
       <!-- Uncomment below if you prefer to use an image logo -->
       <!-- <a href="index.html" class="logo mr-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
 
       <nav class="nav-menu d-none d-lg-block">
         <ul>
-          <li class="active"><a href="index.html">Home</a></li>
+          <li class="active"><a href="{{url('welcome')}}">Home</a></li>
           <li><a href="#about">About</a></li>
           <li><a href="#services">Services</a></li>
 <!--           <li><a href="#portfolio">Portfolio</a></li> -->
